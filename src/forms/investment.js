451b7a880f8a05790c7659478104a3708b588ed6
Jim Smith <jim.smith@digital.trade.gov.uk>
2017-02-10 16:15:03 +0000

    minor reformat

--- src/forms/investment.js
+++ src/forms/investment.js
@@ -1,5 +1,4 @@
-
-const searchfield =document.querySelector("#inv-search");
+const searchfield = document.querySelector("#inv-search");
 const resultsdiv = document.querySelector("#inv-results");
 
 const trade = require('@uktrade/trade_elements').elementstuff
@@ -7,30 +6,29 @@ const trade = require('@uktrade/trade_elements').elementstuff
 const companyTemplate = "<span id='[[conameid]]' class='clickable'>[[name]]</span><span id='[[headclose]]' class='hidden clickable' style='float:right'>Close</span>" +
   "<br>[[address]]</div>" +
   "<div class='hidden subresults'>" +
-  "<table class='table--key-value  table--readonly'>"+
+  "<table class='table--key-value  table--readonly'>" +
   "<tr>" +
   "<td>Company Type</td>" +
   "<td>[[company_type]]</td>" +
-  "</tr>"+
+  "</tr>" +
   "<tr>" +
   "<td>Investment in the UK</td>" +
   "<td>[[investment_projects]]</td>" +
-  "</tr>"+
+  "</tr>" +
   "<tr>" +
   "<td>Account tier</td>" +
   "<td>[[account_tier]]</td>" +
-  "</tr>"+
+  "</tr>" +
   "<tr>" +
   "<td>Account Manager in the UK</td>" +
   "<td>[[account_manager]]</td>" +
-  "</tr>"+
-  "</table>"+
+  "</tr>" +
+  "</table>" +
   "<div class='save-bar'>" +
   "<button class='button button--save' type='submit'>Choose company</button>" +
   "<a class='button-link button--cancel js-button-cancel' href='#' id='[[bottomclose]]'>Close</a>" +
   "</div>" +
   "</div>";
-
 
 
 function ifdef(thing, replacer) {
@@ -44,7 +42,7 @@ function updateSearchField(res) {
   resultsdiv.innerHTML = ""
   trade.removeClass(document.querySelector("#new_co_nagger"), "hidden")
   const companies = JSON.parse(res.response)
-  Object.keys(companies).forEach(function(key){
+  Object.keys(companies).forEach(function (key) {
     let co = companies[key]
     let d = document.createElement("div")
     d.id = "inv-res-" + key
@@ -62,36 +60,36 @@ function updateSearchField(res) {
     d.innerHTML = divtext;
     resultsdiv.appendChild(d);
 
-    setTimeout(function() {
-        let resblock = document.querySelector("#inv-res-" + key + " > .subresults");
-        let coname = document.querySelector("#name-" + key);
-        let headcloser = document.querySelector("#headclose-" + key);
-        document.querySelector("#inv-res-" + key).addEventListener("click", function() {
-            trade.removeClass(headcloser, "hidden")
-            trade.removeClass(coname, "clickable")
-            trade.addClass(coname, "unclickable")
-            trade.show(resblock)
-        }, true)
-        document.querySelector("#close-" + key).addEventListener("click", function() {
-          trade.addClass(coname, "clickable")
-          trade.addClass(headcloser, "hidden")
-          trade.removeClass(coname, "unclickable")
-          trade.hide(resblock)
-        }, true)
-      document.querySelector("#headclose-" + key).addEventListener("click", function() {
+    setTimeout(function () {
+      let resblock = document.querySelector("#inv-res-" + key + " > .subresults");
+      let coname = document.querySelector("#name-" + key);
+      let headcloser = document.querySelector("#headclose-" + key);
+      document.querySelector("#inv-res-" + key).addEventListener("click", function () {
+        trade.removeClass(headcloser, "hidden")
+        trade.removeClass(coname, "clickable")
+        trade.addClass(coname, "unclickable")
+        trade.show(resblock)
+      }, true)
+      document.querySelector("#close-" + key).addEventListener("click", function () {
         trade.addClass(coname, "clickable")
         trade.addClass(headcloser, "hidden")
         trade.removeClass(coname, "unclickable")
         trade.hide(resblock)
       }, true)
-    },10)
+      document.querySelector("#headclose-" + key).addEventListener("click", function () {
+        trade.addClass(coname, "clickable")
+        trade.addClass(headcloser, "hidden")
+        trade.removeClass(coname, "unclickable")
+        trade.hide(resblock)
+      }, true)
+    }, 10)
 
   })
 }
 
 function searchCos(term) {
   const companyRequest = new XMLHttpRequest();
-  companyRequest.onreadystatechange = function() {
+  companyRequest.onreadystatechange = function () {
     if (companyRequest.readyState === 4) {
       updateSearchField(companyRequest);
     }
@@ -101,6 +99,6 @@ function searchCos(term) {
   companyRequest.send();
 }
 
-searchfield.addEventListener("keyup", function() {
+searchfield.addEventListener("keyup", function () {
   searchCos(searchfield.value)
 }, true);
